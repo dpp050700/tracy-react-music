aa1cd4805cac531e9b3954f2e603af2077a9b75d
daipengpeng <16602124116@163.com>
2021-03-18 10:23:37 +0800

    init: eslint

---
+++ .eslintrc.js
@@ -0,0 +1,55 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+    jest: true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/no-unused-vars': [2, { args: 'none' }],
+        'no-unused-expressions': 'off',
+        '@typescript-eslint/no-unused-expressions': 2,
+      },
+    },
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/require-default-props': 0,
+    'no-use-before-define': 0,
+    '@typescript-eslint/no-use-before-define': 2,
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.tsx', '.ts'] }],
+    'import/extensions': 0,
+    'react/button-has-type': 0,
+    'react/jsx-props-no-spreading': 0,
+    'no-console': 0,
+  },
+};
