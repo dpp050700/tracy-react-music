ce5822878ec1e4e3cdda1163b1c5b69f64b75cae
daipengpeng <16602124116@163.com>
2021-04-09 14:01:59 +0800

    优化 axios 拦截

--- src/request/axios.ts
+++ src/request/axios.ts
@@ -1,6 +1,9 @@
 import axios from 'axios';
+import { createHashHistory } from 'history';
+import Toast from '../base/Toast/toast';
 
-// export const baseUrl = 'http://1.116.70.16:8880/';
+const history = createHashHistory();
+
 export const baseUrl = 'http://cijianshaonian.com:8880/';
 
 const axiosInstance = axios.create({
@@ -9,9 +12,24 @@ const axiosInstance = axios.create({
 axiosInstance.defaults.withCredentials = true;
 
 axiosInstance.interceptors.response.use(
-  res => res.data,
-  err => {
-    console.log(err, '网络错误');
+  res => {
+    const { data, status } = res;
+    if (status === 200 && data.code === 200) {
+      return data;
+    }
+    return res.data;
+  },
+  error => {
+    const {
+      response: { data },
+    } = error;
+    const { code, msg } = data;
+    if (code === 301) {
+      history.push('/login');
+      return error;
+    }
+    Toast.error({ content: msg });
+    return error;
   },
 );
 
